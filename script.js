26096644c705bb9332b3aea9b39ec4ffab13a7a4
D Kumar <deepkumar0501@gmail.com>
2025-09-03 22:12:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ arr.forEach(button => {
             string = eval(string);
             input.value = string; 
         }
-
+ 
         else if(e.target.innerHTML == 'AC'){ 
             string = "";
             input.value = string;
@@ -26,6 +26,7 @@ arr.forEach(button => {
     })
 
 })
+
 
 
 
