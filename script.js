bc9e348bfde92c27e70e490d9f4234fcff803ddd
D Kumar <deepkumar0501@gmail.com>
2025-09-06 23:13:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let string = "";
 let arr = Array.from(buttons);     
 arr.forEach(button => {  
     button.addEventListener('click', (e) =>{
+        let deep emper
         if(e.target.innerHTML == '='){ 
             string = eval(string);
             input.value = string; 
@@ -26,6 +27,7 @@ arr.forEach(button => {
     })
 
 })
+
 
 
 
