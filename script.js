fd10e1e76618ce5937f6d9361e44d1eb5e69f6e6
Prometheus <167355459+prometheus0717@users.noreply.github.com>
2025-10-26 23:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ arr.forEach(button => {
             string = eval(string);          
             input.value = string;      
         }   
-   
+    
         else if(e.target.innerHTML == 'ACT'){ 
             string = ""; 
             input.value = string;
@@ -27,6 +27,7 @@ arr.forEach(button => {
     })
 
 })
+
 
 
 
