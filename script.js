4cfbaea9bed29802d803509d94754d64f6e1827f
Prometheus <167355459+prometheus0717@users.noreply.github.com>
2025-10-27 23:43:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');     
        
 let string = "";      
-let arr = Array.from(buttons);                   
+let arr = Array .from(buttons);                   
 arr.forEach(button => {             
     button.addEventListener('click', (e) =>{              
                     
@@ -32,6 +32,7 @@ arr.forEach(button => {
 
 
  
+
 
 
 
