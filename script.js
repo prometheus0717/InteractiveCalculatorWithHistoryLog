890de44c4dd7bba023c328fcc75d9340abdd4a14
Prometheus <167355459+prometheus0717@users.noreply.github.com>
2025-11-06 23:59:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let buttons = document.querySelectorAll('button');
 let string = "";       
 let arr = Array.from(buttons);                   
 arr.forEach(button => {             
-    button.addEventListener('click', (e) =>{               
+    button.addEventListener('click', (e) =>{                
                     
         if(e.target.innerHTML == '='){            
             string = eval(string);          
@@ -39,6 +39,7 @@ arr.forEach(button => {
 
 
  
+
 
 
 
